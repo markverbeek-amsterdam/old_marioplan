b2971782cc9ec8cf2cebd61ba9a144d4ea552b09
markverbeek-amsterdam <markverbeek@mac.com>
2021-01-11 23:19:20 +0100

    firebase setup

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux'
 class Dashboard extends Component {
   render() {
     
-    // console.log(this.props);
+    console.log(this.props);
     const { projects } = this.props;
     
     return (
