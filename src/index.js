cdd2018200b5433e0059f265fa4d0019aa02bf78
markverbeek-amsterdam <markverbeek@mac.com>
2021-01-14 05:46:11 +0100

    connect to firestore

--- src/index.js
+++ src/index.js
@@ -7,17 +7,40 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { reduxFirestore } from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import { createFirestoreInstance, getFirestore } from 'redux-firestore'
+// import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig'
+import firebase from 'firebase/app'
 
-const store = createStore(rootReducer,
+const store = createStore(
+  rootReducer,
   compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reactReduxFirebase(fbConfig), // redux binding for firebase
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    // reactReduxFirebase(fbConfig), // redux binding for firebase
+
+    // More on the error
+    //https://github.com/prescottprue/react-redux-firebase/issues/620
+
     reduxFirestore(fbConfig) // redux bindings for firestore
   )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
